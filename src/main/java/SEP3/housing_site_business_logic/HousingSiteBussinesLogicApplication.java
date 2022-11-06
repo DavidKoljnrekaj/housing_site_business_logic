5eff95686039865c66ec5fbc28f3bb35fa5afeae
DavidKoljnrekaj <koljnrekajdavid@gmail.com>
2022-11-06 22:45:09 +0100

    Business logic server creation

---
+++ src/main/java/SEP3/housing_site_business_logic/HousingSiteBussinesLogicApplication.java
@@ -0,0 +1,13 @@
+package SEP3.housing_site_business_logic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingSiteBussinesLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingSiteBussinesLogicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SEP3/housing_site_business_logic/HousingSiteBussinesLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package SEP3.housing_site_business_logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingSiteBussinesLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
